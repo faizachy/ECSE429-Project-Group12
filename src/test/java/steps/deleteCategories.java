abca5a1815c0ca8152599dfc0c7cc8180e99ded5
nourktaily <98048051+nourktaily@users.noreply.github.com>
2024-11-04 18:09:01 -0500

    Update deleteCategories.java

--- src/test/java/steps/deleteCategories.java
+++ src/test/java/steps/deleteCategories.java
@@ -1,12 +1,69 @@
 package steps;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class deleteCategories {
+    private HttpClient client;
+    public static String categoryId = "0";
+
+
+    @BeforeEach
+    public void setup() throws IOException, InterruptedException {
+        client = HttpClient.newHttpClient();
+        categoryId = createCategoryAndGetId();
+
+    }
+
+    @AfterEach
+    public void restore() throws IOException, InterruptedException {
+        deleteCategoryById(categoryId);
+    }
+
+    private static void deleteCategoryById(String categoryId) throws IOException, InterruptedException {
+        String id = createCategoryAndGetId();
+        HttpRequest deleteRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:4567/categories/" + id))
+                .DELETE()
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> deleteResponse = client.send(deleteRequest, HttpResponse.BodyHandlers.ofString());
+
+        assertEquals(200, deleteResponse.statusCode(), "Failed to delete project with id: " + categoryId);
+    }
+
+    private static String createCategoryAndGetId() throws IOException, InterruptedException {
+        String categoryRequestBody = "{ \"title\": \"Category Title\", \"description\": \"Category Description\" }";
+        HttpRequest categoryRequest = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:4567/categories"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(categoryRequestBody))
+                .build();
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> categoryResponse = client.send(categoryRequest, HttpResponse.BodyHandlers.ofString());
+        assertEquals(201, categoryResponse.statusCode());
+
+        String responseBody = categoryResponse.body();
+        String id = new ObjectMapper().readTree(responseBody).get("id").asText();
+
+        return id;
+    }
+
     private int responseStatus;
-    private String categoryId;
+
 
     static {
         baseURI = "http://localhost:4567";
